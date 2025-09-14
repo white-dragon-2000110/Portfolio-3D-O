edc6cc294f618b90a7fbfb734074f01ff3d4ed10
white-dragon-2000110 <whitedragon2000110@gmail.com>
2025-09-14 12:26:49 -0700

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
@@ -10,19 +9,15 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
-import Banner from "./components/banner";
 import Footer from "./components/footer";
 
 // App
 const App = () => {
-  const [hide, setHide] = useState(true);
-
   return (
     <BrowserRouter>
-      <Banner hide={hide} setHide={setHide} />
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar hide={hide} />
+          <Navbar />
           <Hero />
         </div>
         <About />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,8 @@ import { NAV_LINKS } from "../constants";
 import { styles } from "../styles";
 import { cn } from "../utils/lib";
 
-type NavbarProps = {
-  hide: boolean;
-};
-
 // Navbar
-export const Navbar = ({ hide }: NavbarProps) => {
+export const Navbar = () => {
   // state variables
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
@@ -36,7 +32,7 @@ export const Navbar = ({ hide }: NavbarProps) => {
       className={cn(
         styles.paddingX,
         "w-full flex items-center py-5 fixed top-0 z-20 bg-primary",
-        isAtBottom || hide ? "mt-0" : "mt-20"
+        isAtBottom ? "mt-0" : "mt-0"
       )}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
